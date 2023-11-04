231cc730dcbac600f6ac6843a29a77425a2fe1e4
Ismail Demirbilek <ce.demirbilek@gmail.com>
2023-11-05 02:07:17 +0300

    add unenroll

--- app/auth/profile/totp/page.tsx
+++ app/auth/profile/totp/page.tsx
@@ -1,15 +1,22 @@
+import { ConfirmDialog } from '@/components/ConfirmDialog';
 import { List } from '@/components/List';
 import { ListItem } from '@/components/ListItem';
 import { ListItemText } from '@/components/ListItemText';
 import { ListSubheader } from '@/components/ListSubheader';
 import { PageTitle } from '@/components/PageTitle';
 import { TOTPForm } from '@/components/TOTPForm';
+import { Alert } from '@/components/ui/alert';
 import { Button } from '@/components/ui/button';
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { Trash2 } from 'lucide-react';
+import { createServerActionClient, createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { FileWarning, Trash2 } from 'lucide-react';
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-export default async function TOTP() {
+
+
+export default async function TOTP(props: {
+  searchParams: Record<string, string>;
+}) {
   const cookieStore = cookies();
   const supabase = createServerComponentClient({
     cookies: () => cookieStore,
@@ -20,28 +27,73 @@ export default async function TOTP() {
   });
 
   const { data } = await supabase.auth.mfa.listFactors();
-  console.log(enrollData);
 
+  const errors = [];
+  if (error) errors.push(error.message);
+  if (props.searchParams.error) errors.push(props.searchParams.error);
 
   return (
     <main>
       <PageTitle primary="Manage 2FA TOTP Authenticator App" />
-      {error && <div className="text-destructive py-2">{error.message}</div>}
+      {errors.length > 0 && (
+        <Alert variant="destructive" className="mb-4">
+          <FileWarning />
+          <div className='ml-2'>
+            {errors.map((error) => <div key={error}>{error}</div>)}
+          </div>
+        </Alert>
+      )}
+
       {enrollData && <TOTPForm factorId={enrollData.id} qrCode={enrollData.totp.qr_code} />}
 
       <ListSubheader className="mt-6 mb-2">
         Current Factors
       </ListSubheader>
       <List className="w-full max-w-md">
-        {data && data.totp.map((totp) => (
-          <ListItem key={totp.id}>
-            <ListItemText primary={totp.factor_type} secondary={`${totp.status}`} />
-            <Button className="bg-destructive p-0 rounded-full w-8 h-8">
-              <Trash2 size={16} />
-            </Button>
-          </ListItem>
-        ))}
+        {data && data.totp.map((totp) => <TOTPListItem key={totp.id} {...totp} />)}
       </List>
     </main>
+  );
+}
+
+function TOTPListItem(props: {
+  id: string,
+  factor_type: string,
+  status: string,
+}) {
+  async function unenroll(fd: FormData) {
+    'use server';
+    const cookieStore = cookies();
+    const supabase = createServerActionClient({
+      cookies: () => cookieStore,
+    });
+
+    const { error } = await supabase.auth.mfa.unenroll({
+      factorId: fd.get('factorId') as string,
+    });
+
+    if (error) {
+      redirect('/auth/profile/totp?error=' + error.message);
+    }
+  }
+
+  return (
+    <ListItem key={props.id}>
+      <ListItemText primary={props.factor_type} secondary={`${props.status}`} />
+      <ConfirmDialog
+        title="Unenroll"
+        message="TOTP will be removed from your account. Are you sure?"
+        onConfirm={unenroll}
+        hiddenFields={{ factorId: props.id }}
+      >
+        <Button
+          type="button"
+          variant="ghost"
+          className="text-destructive"
+        >
+          <Trash2 size={16} />
+        </Button>
+      </ConfirmDialog>
+    </ListItem>
   );
 }
